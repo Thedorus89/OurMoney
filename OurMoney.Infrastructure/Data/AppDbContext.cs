87c47a9c9232f0656d4c03fa311abfbfe6338e1b
Thedorus89 <theodoredanang@gmail.com>
2025-01-29 18:42:59 +0700

    edit db context

---
+++ OurMoney.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using OurMoney.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OurMoney.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
